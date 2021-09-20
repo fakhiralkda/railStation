11e5ac5a3adb2c54589c7061edef3e506ce62757
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-09-21 04:20:14 +0700

    add new repo

--- start.sh
+++ start.sh
@@ -26,9 +26,11 @@ chmod 600 ${GIT_CREDS}
 # clone repos
 git clone https://github.com/rzlamrr/aria -b gh --quiet
 git clone https://github.com/fakhiralkda/redditGram --quiet rgram
+git clone https://github.com/fakhiralkda/hyconbot --quiet
 
 # run it
 screen -dmS A bash -c "cd aria; bash build.sh"
 screen -dmS B bash -c "cd rgram; pip3 install -r requirements.txt; bash loop.sh"
+screen -dmS C bash -c "cd hyconbot; pip3 install -r requirements.txt; python3 -m hycon"
 
 rm -f ${GIT_CREDS}
