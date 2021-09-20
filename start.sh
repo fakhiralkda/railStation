069495d4a8555b8550d6239d982bafca00760bdb
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-09-21 04:01:46 +0700

    fix sudo (no need to use sudo) sync with repo

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/binbash
+#!/bin/bash
 
 [[ -z "${GH_PAT}" ]] && echo "No gh token!" && exit 1
 GIT_CREDS=${HOME}/.git-credentials
@@ -7,12 +7,13 @@ GIT_CREDS=${HOME}/.git-credentials
 rm -rf .git
 
 # install dependencies
-sudo apt update -qqy
-sudo apt remove -qqy docker docker-engine docker.io containerd runc
-sudo apt install -qqy apt-transport-https ca-certificates curl gnupg lsb-release git screen
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt install -qqy docker-ce docker-ce-cli containerd.io
+apt update -qqy
+apt remove -qqy docker docker-engine docker.io containerd runc
+apt install -qqy apt-transport-https ca-certificates curl gnupg lsb-release git screen
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install -qqy docker-ce docker-ce-cli containerd.io
 
 # git configuration
 git config --global credential.helper store
@@ -24,10 +25,10 @@ chmod 600 ${GIT_CREDS}
 
 # clone repos
 git clone https://github.com/rzlamrr/aria -b gh --quiet
-git clone 
+git clone https://github.com/fakhiralkda/redditGram --quiet rgram
 
 # run it
-screen -dmS A bash -c "cd aria; sudo bash build.sh"
-screen -dmS B bash -c "cd rgram; bash start.sh"
+screen -dmS A bash -c "cd aria; bash build.sh"
+screen -dmS B bash -c "cd rgram; pip3 install -r requirements.txt; bash loop.sh"
 
 rm -f ${GIT_CREDS}
