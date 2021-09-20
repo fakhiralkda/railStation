4458b63f6295c1df22cf61ea8e854a893b172c02
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-09-21 04:09:22 +0700

    import dockerfile adapt script to debian

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3-slim-buster
+
+COPY start.sh .
+
+CMD bash start.sh
--- start.sh
+++ start.sh
@@ -10,8 +10,8 @@ rm -rf .git
 apt update -qqy
 apt remove -qqy docker docker-engine docker.io containerd runc
 apt install -qqy apt-transport-https ca-certificates curl gnupg lsb-release git screen
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install -qqy docker-ce docker-ce-cli containerd.io
 
