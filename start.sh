9e1f2db52244e67af48e6dc8427e3857c5177c30
dαvιѕтα <rzlamrr.dvst@protonmail.com>
2021-09-22 20:13:03 +0700

    run chibot
    Maybe:v

--- start.sh
+++ start.sh
@@ -12,6 +12,7 @@ git config --global user.email rzlamrr.dvst@protonmail.com
 
 # clone repos
 git clone https://rzlamrr:${GH_PAT}@github.com/rzlamrr/usergex
+git clone https://github.com/rzlamrr/aria
 git clone https://github.com/fakhiralkda/redditGram rgram
 git clone https://github.com/fakhiralkda/hyconbot
 
@@ -24,6 +25,13 @@ screen -dmS C bash -c "cd hyconbot; pip3 install -r requirements.txt; python3 -m
 wget -q -O requirements.txt https://raw.githubusercontent.com/UsergeTeam/Userge/alpha/requirements.txt
 pip install -q -r requirements.txt
 wget -q -O /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker-ce.list
+apt update -qqy
+apt install -qqy docker-ce docker-ce-cli containerd.io
+
+screen -dmS C bash -c "cd aria; docker build . -t chi; docker run -p 80:80 chi"
 
 cd usergex
 bash run
